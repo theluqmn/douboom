ac602643e17f93fb7ee861dce330a4f9dea83a6e
luq-mne <luqman.yazrie@outlook.com>
2024-12-26 23:25:09 +0800

    for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
+  base: "/douboom/",
   plugins: [solidPlugin()],
   server: {
     port: 3000,
