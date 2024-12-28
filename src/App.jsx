323c80d95e4b9c2d737fb3dc39a98ba730b1b4e8
luq-mne <luqman.yazrie@outlook.com>
2024-12-28 09:48:46 +0800

    gonna work on the doubloon stats

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Doubloon from "./Doubloon"
+
 export default function App() {
     return (
         <div class>
             <header
-            class="p-2 bg-[rgb(241,196,15)] items-center flex flex-col fixed z-10 w-full duration-500">
+            class="p-2 bg-white items-center flex flex-col fixed z-10 w-full duration-500">
                 <div class="lg:w-7/12 flex flex-row items-center gap-2">
+                    <img src="https://highseas.hackclub.com/doubloon.svg" alt="Doubloon" class="w-8 h-8" />
+
                     <h1 class="text-2xl text-black hover:font-bold duration-500">
                         Douboom
                     </h1>
@@ -11,7 +15,7 @@ export default function App() {
             </header>
 
             <div class="pt-32 p-4 md:p-4 md:pt-32 lg:w-7/12 items-center mx-auto space-y-3 text-sm lg:text-base">
-                <h1 class="text-[rgb(241,196,15)] text-3xl">
+                <h1 class="text-[rgb(241,196,15)] text-5xl">
                     My 2024/2025 Hack Seas Journey
                 </h1>
 
@@ -21,7 +25,7 @@ export default function App() {
                     of this opportunity and prove my competence.
                 </p>
 
-                <div class="pt-16 flex flex-col gap-2">
+                <div class="pt-64 flex flex-col gap-4">
                     <h1 class="text-[rgb(241,196,15)] text-3xl">
                         Projects
                     </h1>
@@ -32,18 +36,20 @@ export default function App() {
                     </div>
                 </div>
 
-                <div class="pt-16 flex flex-col gap-2">
+                <div class="pt-16 flex flex-col gap-4">
                     <h1 class="text-[rgb(241,196,15)] text-3xl">
                         Items to redeem
                     </h1>
 
-                    <h2 class="text-gray-100 text-xl">
-                        Doubloon goal: 3,000
-                    </h2>
+                    <div class="flex flex-col gap-2">
+                        <h2 class="text-gray-100 text-xl">
+                            Doubloon goal: 3,000
+                        </h2>
 
-                    <h2 class="text-gray-100 text-xl">
-                        Rank goal: Top 10
-                    </h2>
+                        <h2 class="text-gray-100 text-xl">
+                            Rank goal: Top 10
+                        </h2>
+                    </div>
 
                     <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
                         <DoubloonCard title="Hack Club MicroSD Card" doubloons="32" image="https://noras-secret-cdn.hackclub.dev/shop/microsd.png/" />
---
+++ src/Doubloon.jsx
@@ -0,0 +1,9 @@
+export default function Doubloon() {
+    return (
+        <div>
+            <h1 class="text-xl text-white">
+                Rank
+            </h1>
+        </div>
+    )
+}
